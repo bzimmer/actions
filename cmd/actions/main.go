eb0457d47bc8d7423a24858fd696ea3f377b0cd6
Brian Zimmer <bzimmer@ziclix.com>
2023-04-15 16:56:04 +0200

    upgrade go action

--- cmd/actions/main.go
+++ cmd/actions/main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"os"
 
 	"github.com/bzimmer/actions"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	fmt.Println(actions.Support())
+	app := &cli.App{
+		Name:     "actions",
+		HelpName: "actions",
+		Usage:    "simple command line tool for testing",
+		Action: func(c *cli.Context) error {
+			enc := json.NewEncoder(c.App.Writer)
+			return enc.Encode(actions.Support())
+		},
+	}
+	if err := app.RunContext(context.Background(), os.Args); err != nil {
+		fmt.Printf("%v\n", err)
+		os.Exit(1)
+	}
 	os.Exit(0)
 }
